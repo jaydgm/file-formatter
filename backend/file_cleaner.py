29d5a5de9ba037268597b797c7c3f85ae5fc9eff
jaydgm <rjayd22@gmail.com>
2026-08-11 21:26:36 -0700

    Created logic for creating new excel template file

--- backend/file_cleaner.py
+++ backend/file_cleaner.py
@@ -31,13 +31,34 @@ def select_file2():
     if file2:
         file2_label.config(text=file2)
 
-def combine_files():
+def combine_files(file1, file2):
     if file1 is None and file2 is None:
-        print('Please select both files')
+        print('Waiting for file selection...')
         return
 
-    print('file 1: ', file1)
-    print('file2: ', file2)
+    wb1 = load_workbook(file1)
+    wb2 = load_workbook(file2)
+
+    ws1 = wb1.active
+    ws2 = wb2.active
+
+    data_dict1 = {}
+
+    # need to fix- not trying to get the value while keeping .iter_cols
+    for cell in ws1.iter_cols(min_row=5, max_col=5):
+        cell = ws1.cell(
+            row=6,
+            column=5
+        ).value
+        print(cell)
+
+    cleaned_file = Workbook()
+
+    file_path = r'C:\Users\rjayd\OneDrive\Documents\Excel files\cleaned_file.xlsx'
+    cleaned_file.save(file_path)
+
+    print('created excel file', file_path)
+
 
 # file 1
 tk.Button(
@@ -58,10 +79,11 @@ tk.Button(
 file2_label = tk.Label(root, text="No file selected")
 file2_label.pack()
 
+# lambda runs combine_files once button is clicked
 tk.Button(
     root,
     text='Combine Files',
-    command=combine_files
+    command= lambda: combine_files(file1, file2)
 ).pack(pady=15)
 
 # start tkinter
